fdb2efb519ea0f2a5795ef7b6d30b0802bd29ce3
Pluto <1799718517@qq.com>
2024-04-16 21:58:36 +0800

    2024.4.16

--- leetcode.c
+++ leetcode.c
@@ -821,3 +821,58 @@ bool isValid(char* s) {
         return 0;
     }
 }
+
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
+    int n = numsSize;
+    int queue[n]; //队列
+    int front = 0, rear = -1; //队首 队尾
+    int left = 0, right = 0; //窗口左下标 窗口右下标
+    while (right < n)//窗口右移至终点
+    { 
+        while (rear >= front && nums[right] > queue[rear])
+        {
+            rear--; //维护队列的单调性（非递增），即保证队首元素就是当前窗口的最大值
+        }
+        queue[++rear] = nums[right++]; //入队下一个窗口可能的最大值
+        if (left + k <= right)//窗口大小大于k
+        { 
+            if (nums[left] == queue[front])
+            {
+                front++; //如果最大值已经在窗口的左边，则将它永久出队
+            }
+            else
+            {
+                nums[left] = queue[front]; //否则记录最大值进原数组中
+            }
+            left++; //左框右移
+        }
+    }
+    *returnSize = n - k + 1;
+    return nums; //返回原数组
+}
+
+typedef struct{
+    char string[100001];
+    int top;
+}str;
+
+char* removeDuplicates(char* s) {
+    str *s2=(str*)malloc(sizeof(str));
+    s2->top=0;
+    int i=0;
+    while(s[i]!='\0')
+    {
+        if(s2->top>0&&s[i]==s2->string[s2->top-1])
+        {
+            s2->top--;
+        }
+        else
+        {
+            s2->string[s2->top]=s[i];
+            s2->top++;
+        }
+        i++;
+    }
+    s2->string[s2->top]='\0';
+    return s2->string;
+}
