caa9835c4f54f22b195f0110ad3ca29c4591e946
Pluto <1799718517@qq.com>
2024-05-29 20:33:53 +0800

    2024.5.29

--- leetcode.c
+++ leetcode.c
@@ -946,3 +946,38 @@ public:
         return n;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode *l3=(struct ListNode *)malloc(sizeof(struct ListNode));
+    l3->val=0;
+    struct ListNode *p=l3;
+    int carry = 0;
+    while (l1 != NULL || l2 != NULL || carry != 0)
+    {
+        int sum = carry;
+        if (l1 != NULL)
+        {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL)
+        {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
+        node->val = sum % 10;
+        node->next = NULL;
+        p->next = node;
+        p = p->next;
+    }
+    return l3->next;
+}
