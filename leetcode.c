4870c4fbc9100aee5126a472a92120406f9ee92a
Pluto <1799718517@qq.com>
2024-06-06 20:16:16 +0800

    2024.6.6

--- leetcode.c
+++ leetcode.c
@@ -1013,3 +1013,36 @@ public:
         putRightFork();
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode* head=(struct ListNode* )malloc(sizeof(struct ListNode));
+    struct ListNode* p=head;
+    struct ListNode* p1=list1;
+    struct ListNode* p2=list2;
+    while(p1!=NULL&&p2!=NULL)
+    {
+        if(p1->val<p2->val)
+        {
+            p->next=p1;
+            p1=p1->next;
+        }
+        else
+        {
+            p->next=p2;
+            p2=p2->next;
+        }
+        p=p->next;
+    }
+    p->next = p1 != NULL ? p1 : p2;
+    struct ListNode* tem=head;
+    head=head->next;
+    free(tem);
+    return head;
+}
